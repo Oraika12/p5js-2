c48adf16dae1a9f106646047bd993d396aeb9276
Oraika12 <134251574+Oraika12@users.noreply.github.com>
2024-10-10 07:41:35 +0700

    first commit

---
+++ Praktek1/Praktek1.js
@@ -0,0 +1,13 @@
+function setup() {
+  //membuat canvas dengan ukuran panjang dan lebar 400
+  createCanvas(400, 400);
+}
+
+function draw() {
+  //menambahkan warna menjadi abu-abu
+  background(220);
+  circle(30, 30, 80);
+  circle(30, 70, 80);
+  circle(70, 110, 80);
+  circle(110, 150, 80);
+} 
